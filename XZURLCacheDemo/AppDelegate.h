80b120f78de98897ed21e91c0b00dcf734e7d11b
codeLocker <codeLockers@outlook.com>
2016-12-19 13:42:20 +0800

    Initial Commit

---
+++ XZURLCacheDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XZURLCacheDemo
+//
+//  Created by codeLocker on 2016/12/19.
+//  Copyright © 2016年 codeLocker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
